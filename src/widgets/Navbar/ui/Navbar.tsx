5bfd202397d5cb0502434d196949a7e30386fcfe
Alfino <nikitamalashkin73@gmail.com>
2024-02-08 18:12:30 +0600

    restyle of app

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next'
 import Home from 'shared/assets/icons/home.svg'
 import About from 'shared/assets/icons/about.svg'
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
+import Logo from 'shared/assets/icons/logo.svg'
 import s from './Navbar.module.scss'
 
 interface NavbarProps {
@@ -13,13 +14,19 @@ export const Navbar = ({ className }: NavbarProps) => (
     // const { t } = useTranslation('translation')
 
     <div className={classNames(s.navbar, {}, [className])}>
-        <AppLink theme={AppLinkTheme.PRIMARY} to="/">
-            <Home className={s.home} />
-            {/* alt={t('Главная')} */}
-        </AppLink>
-        <AppLink theme={AppLinkTheme.PRIMARY} to="/about">
-            <About className={s.about} />
-            {/* alt={t('О сайте'} */}
-        </AppLink>
+        <div className={s.logo}>
+            <Logo />
+            <span>social</span>
+        </div>
+        <div className={s.links}>
+            <AppLink theme={AppLinkTheme.PRIMARY} to="/">
+                <Home className={s.home} />
+                {/* alt={t('Главная')} */}
+            </AppLink>
+            <AppLink theme={AppLinkTheme.PRIMARY} to="/about">
+                <About className={s.about} />
+                {/* alt={t('О сайте'} */}
+            </AppLink>
+        </div>
     </div>
 )
